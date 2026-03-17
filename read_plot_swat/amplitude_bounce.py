1b3bffe2151121460da851630c4c635760f01efc
shubham-688 <agr.shubh.agr@gmail.com>
2026-03-17 11:55:16 -0400

    working now; but taup amps are wrong

--- read_plot_swat/amplitude_bounce.py
+++ read_plot_swat/amplitude_bounce.py
@@ -11,18 +11,20 @@ import requests
 from collections import defaultdict
 import math
 import matplotlib.pyplot as plt
+import sys
 ###
-INPUT_FILE = "amps_230402_180411.txt"   # change to your filename
 
 def parse_file(path):
     """
     Parse file, returning a dict:
       grouped[(col0, col1)][phase] = max_abs_amplitude_for_that_phase
     """
-    grouped = defaultdict(lambda: dict())
+    # grouped = defaultdict(lambda: dict())
+    grouped = defaultdict()
 
     with open(path, 'r') as f:
-        for raw in f:
+        lines = f.readlines()
+        for raw in lines[3:]:
             line = raw.strip()
             parts = line.split()
 
@@ -30,17 +32,51 @@ def parse_file(path):
             amp_str = parts[-2]
             amp = float(amp_str)
 
-
-            # grouping key for a "record" (change if you want a different grouping)
-            record_key = (parts[0], parts[1])
+            # grouping key
+            # record_key = (parts[0], parts[1])
 
             # keep the largest absolute amplitude for each phase within the same record
-            prev = grouped[record_key].get(phase)
+            prev = grouped.get(phase)
             if prev is None or abs(amp) > abs(prev):
-                grouped[record_key][phase] = amp
+                grouped[phase] = amp
 
     return grouped
 
+amp_file = "amps_230402_180411.txt"
+grouped = parse_file(amp_file)
+phase_ratios = defaultdict(list)
+amp_ref_abs = abs(grouped['P'])
+for phase, amp in grouped.items():
+
+    ratio = np.round(abs(amp) / amp_ref_abs,3)
+    phase_ratios[phase].append(ratio)
+
+phases = dict(
+    sorted(phase_ratios.items(), key=lambda x: x[1][0], reverse=True))
+
+print(phase_ratios)
+
+plt.figure(figsize=(10, 6))
+
+for i, p in enumerate(phases):
+    y = phase_ratios[p]
+    x = [i + 1] * len(y)  # align points vertically per phase
+
+    color = 'rosybrown' if 'S' in p else 'olivedrab'
+
+    plt.scatter(x, y, marker='D', alpha=0.8,s=55, color=color)
+
+plt.xticks(range(1, len(phases) + 1), phases)
+# plt.yscale('log')
+plt.ylabel("Amplitude P/ phase")
+plt.xlabel("phase")
+plt.title("Phase amplitude ratios")
+
+plt.grid(which='both', linestyle='--', linewidth=0.5, alpha=0.6)
+plt.tight_layout()
+plt.show()
+
+sys.exit()
 taup_path="~/Research/sct_wat/TauP-3.2.0-SNAPSHOT6/bin/taup"
 
 sta=(64.67, -155.88)
@@ -60,21 +96,6 @@ with taup.TauPServer( taup_path=taup_path) as taupserver:
     params.phase('P')
     timeResult = params.calc(taupserver)
 ##
-   82.02    70.0   P        731.39     5.216     22.54    15.79      0.0    82.02   = P       4.2e-05  0.0e+00
-   82.02    70.0   pP       749.39     5.253    157.29    15.90      0.0    82.02   = pP     -3.5e-05  0.0e+00
-   82.02    70.0   sP       757.33     5.245    167.68    15.88      0.0    82.02   = sP      5.3e-05  0.0e+00
-   82.02    70.0   PP       919.67     8.214     37.14    25.37      0.0    82.02   = PP     -2.1e-05  0.0e+00
-   82.02    70.0   PPP     1030.12     8.975     41.28    27.92      0.0    82.02   = PPP     8.9e-06  0.0e+00
-   82.02    70.0   PPP     1038.35     9.851     46.39    30.92      0.0    82.02   = PPP     5.2e-06  0.0e+00
-   82.02    70.0   PPP     1039.38     9.671     45.30    30.29      0.0    82.02   = PPP     3.4e-06  0.0e+00
-   82.02    70.0   PPP     1129.59    13.545     84.59    44.95      0.0    82.02   = PPP     8.4e-07  0.0e+00
-   82.02    70.0   PPP     1131.99    13.407     80.20    44.37      0.0    82.02   = PPP     9.0e-07  0.0e+00
-   82.02    70.0   SP      1386.64    12.309     30.04    39.94      0.0    82.02   = SP      4.0e-05  0.0e+00
-   82.02    70.0   SP      1394.96    13.545     33.43    44.95      0.0    82.02   = SP      1.4e-05  0.0e+00
-   82.02    70.0   SP      1395.81    13.402     33.03    44.35      0.0    82.02   = SP      1.7e-05  0.0e+00
-   82.02    70.0   PSP     1421.77    12.540     67.17    40.85      0.0    82.02   = PSP     1.6e-07  0.0e+00
-   82.02    70.0   PSP     1422.33    12.950     72.14    42.49      0.0    82.02   = PSP     2.8e-05  0.0e+00
-   82.02    70.0   PPP     2366.78     4.596     19.74    13.87      0.0   277.98   = PPP     2.7e-06  0.0e+00
     # for phase in phases:
     #     params.phase(phase)
     #     timeResult = params.calc(taupserver)
