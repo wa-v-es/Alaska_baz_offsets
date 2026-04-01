40e0320f27a7f1d1d95291cff2b0f260bd5f4e50
shubham-688 <agr.shubh.agr@gmail.com>
2026-04-01 16:41:34 -0400

    getting amp from taup server routine for a bunch of strike, dip, rake vals for a few phases.

--- read_plot_swat/amplitude_compare/amplitude_bounce.py
+++ read_plot_swat/amplitude_compare/amplitude_bounce.py
@@ -6,7 +6,7 @@ import sys,re,os
 import glob as glob
 import numpy as np
 # from obspy.taup import TauPyModel
-import taup
+# import taup
 import requests
 from collections import defaultdict
 import math
@@ -42,62 +42,83 @@ def parse_file(path):
 
     return grouped
 
-amp_file = "amps_230402_180411.txt"
-grouped = parse_file(amp_file)
-phase_ratios = defaultdict(list)
-amp_ref_abs = abs(grouped['P'])
-for phase, amp in grouped.items():
+def get_dict_amps(TimeResult):
+    """
+    readfs taup_py output and gets pahse names and amplitudes.
+    """
+    grouped = defaultdict()
+    for a in TimeResult.arrivals:
+        phase=a.phase
+        amp=float(a.amp.factorpsv)
+        grouped[phase] = amp
 
-    ratio = np.round(abs(amp) / amp_ref_abs,3)
-    phase_ratios[phase].append(ratio)
+    return grouped
 
-phases = dict(
-    sorted(phase_ratios.items(), key=lambda x: x[1][0], reverse=True))
 
-print(phase_ratios)
+# amp_file = "amps_230402_180411.txt"
+# taup_path="~/Research/sct_wat/TauP-3.2.0-SNAPSHOT6/bin/taup"
+taup_path="~/Research/sct_wat/TauP/build/install/TauP/bin/taup"
 
-plt.figure(figsize=(10, 6))
+sta=(64.67, -155.88)
+evt=(-4.33,143.16 )   # eq lat, lon -4.33 143.16 70.00
+eventdepth=70  # eq depth
+model="ak135fcont"
 
-for i, p in enumerate(phases):
-    y = phase_ratios[p]
-    x = [i + 1] * len(y)  # align points vertically per phase
+phases=['S','P','PP','SS','SP','PS','sP','pP','sS','pS','PPP']
+# phases=['PP']
+SDR=[45, 90, 0]
+plt.rcParams.update({'font.size': 15})
 
-    color = 'rosybrown' if 'S' in p else 'olivedrab'
+plt.figure(figsize=(12, 6))
 
-    plt.scatter(x, y, marker='D', alpha=0.8,s=55, color=color)
+with taup.TauPServer(taup_path=taup_path) as taupserver:
+    for s in [0,45,90]:
+        for d in [0,45,90]:
+            for r in [0,45,90]:
+
+                params = taup.TimeQuery()
+                params.model(model)
+                params.event(*evt)
+                params.sourcedepth(eventdepth)
+                params.station(*sta)
+                params.phase(phases)
+                params.amp(True)
+                params.mw(7)
+                params.strikediprake([s,d,r])
+                # params.az(20)
+                TimeResult = params.calc(taupserver)
+
+                # print()
+                grouped = get_dict_amps(TimeResult)
+
+                # sys.exit()
+                phase_ratios = defaultdict(list)
+                amp_ref_abs = abs(grouped['P'])
+
+                for phase, amp in grouped.items():
+
+                    ratio = np.round(abs(amp) / amp_ref_abs,3)
+                    phase_ratios[phase].append(ratio)
+
+                # phases = dict(
+                #     sorted(phase_ratios.items(), key=lambda x: x[1][0], reverse=True))
+
+                # print(phase_ratios)
+                for i, p in enumerate(phases):
+                    y = phase_ratios[p]
+                    x = [i + 1] * len(y)  # align points vertically per phase
+
+                    color = 'darkseagreen' if ('S' in p or 's' in p) else 'palevioletred'
+
+                    plt.scatter(x, y, marker='D', alpha=0.7,s=55, color=color)
 
 plt.xticks(range(1, len(phases) + 1), phases)
 # plt.yscale('log')
 plt.ylabel("Amplitude P/ phase")
 plt.xlabel("phase")
-plt.title("Phase amplitude ratios")
+plt.title(f"Phase amp ratios for SDR in 0, 45, 90")
 
 plt.grid(which='both', linestyle='--', linewidth=0.5, alpha=0.6)
 plt.tight_layout()
 # plt.show()
-plt.savefig("230402_180411_amps.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
-
-sys.exit()
-
-taup_path="~/Research/sct_wat/TauP-3.2.0-SNAPSHOT6/bin/taup"
-
-sta=(64.67, -155.88)
-evt=(-4.33,143.16 )   # eq lat, lon -4.33 143.16 70.00
-eventdepth=70  # eq depth
-model="iasp91"
-
-phases=['PP','sP','pP','PPP','SP']
-phases=['PP']
-
-with taup.TauPServer( taup_path=taup_path) as taupserver:
-    params = taup.TimeQuery()
-    params.model(model)
-    params.event(*evt)
-    params.sourcedepth(eventdepth)
-    params.station(*sta)
-    params.phase('P')
-    timeResult = params.calc(taupserver)
-##
-    # for phase in phases:
-    #     params.phase(phase)
-    #     timeResult = params.calc(taupserver)
+plt.savefig("230402_180411_amps_0_45_90.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
