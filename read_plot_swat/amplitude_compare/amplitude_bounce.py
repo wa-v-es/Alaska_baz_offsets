a1f4d29aa86055400700a731c51f21a10ffb427d
shubham-688 <agr.shubh.agr@gmail.com>
2026-04-05 12:34:31 -0400

    added triplication max amp shenanigan for PPP. all vals of SDR.

--- read_plot_swat/amplitude_compare/amplitude_bounce.py
+++ read_plot_swat/amplitude_compare/amplitude_bounce.py
@@ -16,32 +16,23 @@ import sys
 
 def parse_file(path):
     """
-    Parse file, returning a dict:
-      grouped[(col0, col1)][phase] = max_abs_amplitude_for_that_phase
+    wrote when I copied the result ffrom taup_web :]
     """
-    # grouped = defaultdict(lambda: dict())
     grouped = defaultdict()
-
     with open(path, 'r') as f:
         lines = f.readlines()
         for raw in lines[3:]:
             line = raw.strip()
             parts = line.split()
-
             phase = parts[2]
             amp_str = parts[-2]
             amp = float(amp_str)
-
-            # grouping key
-            # record_key = (parts[0], parts[1])
-
             # keep the largest absolute amplitude for each phase within the same record
             prev = grouped.get(phase)
             if prev is None or abs(amp) > abs(prev):
                 grouped[phase] = amp
-
     return grouped
-
+###
 def get_dict_amps(TimeResult):
     """
     readfs taup_py output and gets pahse names and amplitudes.
@@ -50,11 +41,14 @@ def get_dict_amps(TimeResult):
     for a in TimeResult.arrivals:
         phase=a.phase
         amp=float(a.amp.factorpsv)
-        grouped[phase] = amp
+        prev = grouped.get(phase)
+        if prev is None or abs(amp) > abs(prev):
+            if prev != None:
+                print(f"prev:{prev}; phase:{phase}; abs amp{abs(amp)}")
+            grouped[phase] = amp
 
     return grouped
-
-
+####
 # amp_file = "amps_230402_180411.txt"
 # taup_path="~/Research/sct_wat/TauP-3.2.0-SNAPSHOT6/bin/taup"
 taup_path="~/Research/sct_wat/TauP/build/install/TauP/bin/taup"
@@ -67,14 +61,15 @@ model="ak135fcont"
 phases=['S','P','PP','SS','SP','PS','sP','pP','sS','pS','PPP']
 # phases=['PP']
 SDR=[45, 90, 0]
+#rake −180° and 180°
 plt.rcParams.update({'font.size': 15})
 
 plt.figure(figsize=(12, 6))
 
 with taup.TauPServer(taup_path=taup_path) as taupserver:
-    for s in [0,45,90]:
+    for s in [0,45,90,135,180]:
         for d in [0,45,90]:
-            for r in [0,45,90]:
+            for r in np.arange(-180, 181, 45):
 
                 params = taup.TimeQuery()
                 params.model(model)
@@ -87,11 +82,8 @@ with taup.TauPServer(taup_path=taup_path) as taupserver:
                 params.strikediprake([s,d,r])
                 # params.az(20)
                 TimeResult = params.calc(taupserver)
-
-                # print()
                 grouped = get_dict_amps(TimeResult)
 
-                # sys.exit()
                 phase_ratios = defaultdict(list)
                 amp_ref_abs = abs(grouped['P'])
 
@@ -100,25 +92,21 @@ with taup.TauPServer(taup_path=taup_path) as taupserver:
                     ratio = np.round(abs(amp) / amp_ref_abs,3)
                     phase_ratios[phase].append(ratio)
 
-                # phases = dict(
-                #     sorted(phase_ratios.items(), key=lambda x: x[1][0], reverse=True))
-
-                # print(phase_ratios)
                 for i, p in enumerate(phases):
                     y = phase_ratios[p]
-                    x = [i + 1] * len(y)  # align points vertically per phase
-
+                    x = [i + 1] * len(y)
                     color = 'darkseagreen' if ('S' in p or 's' in p) else 'palevioletred'
-
-                    plt.scatter(x, y, marker='D', alpha=0.7,s=55, color=color)
+                    plt.scatter(x, y, marker='D', alpha=0.2,s=55, color=color)
 
 plt.xticks(range(1, len(phases) + 1), phases)
 # plt.yscale('log')
 plt.ylabel("Amplitude P/ phase")
 plt.xlabel("phase")
-plt.title(f"Phase amp ratios for SDR in 0, 45, 90")
+plt.title(f"Phase amp ratios for all SDR Δ45")
 
 plt.grid(which='both', linestyle='--', linewidth=0.5, alpha=0.6)
 plt.tight_layout()
-# plt.show()
-plt.savefig("230402_180411_amps_0_45_90.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
+plt.show()
+# plt.savefig("230402_180411_amps_all45_m.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
+#########
+##
