f3b6a61bfd67b9d8e8227be22b80e34d6e956632
shubham-688 <agr.shubh.agr@gmail.com>
2026-04-08 11:46:05 -0400

    added bit in 'get_phase_ratio' to estimate amp ratio wrt P explosion!

--- read_plot_swat/amplitude_compare/amplitude_bounce.py
+++ read_plot_swat/amplitude_compare/amplitude_bounce.py
@@ -37,7 +37,7 @@ def parse_file(path):
 ###
 def get_dict_amps(TimeResult):
     """
-    readfs taup_py output and gets pahse names and amplitudes.
+    readfs taup_py output and gets phase names and amplitudes.
     """
     grouped = defaultdict()
     for a in TimeResult.arrivals:
@@ -58,23 +58,35 @@ def get_phase_ratio(taupserver,model,evt,eventdepth,sta,phases,s,d,r,):
     params.event(*evt)
     params.sourcedepth(eventdepth)
     params.station(*sta)
-    params.phase(phases)
+    params.phase('P')
     params.amp(True)
     params.mw(7)
+    TimeResult = params.calc(taupserver)
+    # get amplitude of P for explosion
+    grouped = get_dict_amps(TimeResult)
+    amp_ref_exp_abs = abs(grouped['P'])
+
+    # now get amplitudfe of all phases for sdr vals
+    params.phase(phases)
     params.strikediprake([s,d,r])
     # params.az(20)
     TimeResult = params.calc(taupserver)
     grouped = get_dict_amps(TimeResult)
+    amp_ref_abs = abs(grouped['P'])
 
     phase_ratios = defaultdict(list)
-    amp_ref_abs = abs(grouped['P'])
+    phase_ratios_exp = defaultdict(list)
 
     for phase, amp in grouped.items():
 
         ratio = np.round(abs(amp) / amp_ref_abs,3)
-        phase_ratios[phase].append(ratio)
+        ratio_exp = np.round(abs(amp) / amp_ref_exp_abs,3)
 
-    return phase_ratios
+        phase_ratios[phase].append(ratio)
+        phase_ratios_exp[phase].append(ratio_exp)
+
+
+    return grouped,phase_ratios,phase_ratios_exp,amp_ref_exp_abs
 
 ####
 # amp_file = "amps_230402_180411.txt"
@@ -94,18 +106,22 @@ plt.rcParams.update({'font.size': 15})
 
 plt.figure(figsize=(12, 6))
 all_phaseR = defaultdict(list)
+
+##
 with taup.TauPServer(taup_path=taup_path) as taupserver:
+
     for s in np.arange(0, 181, 45):
         for d in np.arange(0, 91, 45):
             for r in np.arange(-180, 181, 45):
-                phase_ratios= get_phase_ratio(taupserver,model,evt,eventdepth,sta,phases,s,d,r)
-                for ph, vals in phase_ratios.items():
+                amps,phase_ratios,phase_ratios_exp,amp_P_exp_abs= get_phase_ratio(taupserver,model,evt,eventdepth,sta,phases,s,d,r)
+                #### change here for Explosion P vs normal P.
+                for ph, vals in phase_ratios_exp.items():
                     all_phaseR[ph].extend(vals)
                 for i, p in enumerate(phases):
-                    y = phase_ratios[p]
+                    y = phase_ratios_exp[p]
                     x = [i + 1] * len(y)
                     color = 'darkseagreen' if ('S' in p or 's' in p) else 'palevioletred'
-                    plt.scatter(x, y, marker='D', alpha=0.2,s=55, color=color)
+                    plt.scatter(x, y, marker='X', alpha=0.2,s=55, color=color)
 
 # keep uniques vals in
 for ph in all_phaseR:
@@ -114,22 +130,33 @@ for ph in all_phaseR:
 min_phaseR = {ph: (float(np.min(v)) if v else None) for ph, v in all_phaseR.items()}
 max_phaseR = {ph: (float(np.max(v)) if v else None) for ph, v in all_phaseR.items()}
 mean_phaseR = {ph: (float(np.round(np.mean(v),3)) if v else None) for ph, v in all_phaseR.items()}
+median_phaseR = {ph: (float(np.round(np.median(v),3)) if v else None) for ph, v in all_phaseR.items()}
+
 print('mean Phase Ratios:\n', mean_phaseR)
+print('median Phase Ratios:\n', median_phaseR)
+
 
 plt.xticks(range(1, len(phases) + 1), phases)
 # plt.yscale('log')
 plt.ylabel("Amplitude P/ phase")
 plt.xlabel("phase")
-plt.title(f"Phase amp ratios for all SDR Δ45")
+# plt.title(f"Phase amp P exp")
+plt.title(f"Phase amp ratios wrt P exp; Δ20")
+
 plt.grid(which='both', linestyle='--', linewidth=0.5, alpha=0.6)
 plt.tight_layout()
+# plt.savefig("230402_180411_amps_expl.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
+plt.savefig("230402_180411_amps_wrt_Pexp_20.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
 # plt.show()
-# plt.savefig("230402_180411_amps_all45_m.png", dpi=500, bbox_inches='tight', pad_inches=0.1)
-# sys.exit()
+plt.close()
+sys.exit()
 
 #########
 ## bit for getting actaul phases from a swat output
 csv_path='../230402_180411_S_10baz.csv'
+# csv_path='../230402_180411_reso_dup.csv'
+
+
 df = pd.read_csv(csv_path)
 bounce=["pP","PP",'sP','SP','SS','sS']
 s_phases=['sP','SP','SS','sS','s','S','Sed']
@@ -139,6 +166,8 @@ print('Len of read csv:',len(df),'\n')
 df["n_bounces"] = df["evt_scat_phase"].isin(bounce).astype(int) + df["sta_scat_phase"].isin(bounce).astype(int)
 df["n_Sphases"] = df["evt_scat_phase"].isin(s_phases).astype(int) + df["sta_scat_phase"].isin(s_phases).astype(int)
 ##
+# dups_all = df[df.duplicated(keep=False)]
+
 df = df.drop_duplicates().reset_index(drop=True)
 print('Len of unique scats:',len(df),'\n')
 ##
@@ -170,10 +199,10 @@ def alias(ph: str) -> str:
     return phase_alias.get(ph, ph)
 
 # vector for row/col phase amps
-row_w = np.array([float(mean_phaseR.get(alias(ph), 1.0)) for ph in mat2.index], dtype=float)      # (nrow,)
-col_w = np.array([float(mean_phaseR.get(alias(ph), 1.0)) for ph in mat2.columns], dtype=float)   # (ncol,)
+row_w = np.array([float(median_phaseR.get(alias(ph), 1.0)) for ph in mat2.index], dtype=float)      # (nrow,)
+col_w = np.array([float(median_phaseR.get(alias(ph), 1.0)) for ph in mat2.columns], dtype=float)   # (ncol,)
 
-mat_color =  row_w[:, None] * col_w[None, :]
+mat_color = row_w[:, None] * col_w[None, :]
 
 color_by_amp=True
 
@@ -183,9 +212,9 @@ if color_by_amp:
         mat_color, #mat2 #np.log10(mat + 1)
         x=mat2.columns,
         y=mat2.index,
-        labels=dict(x="sta_scat_phase", y="evt_scat_phase", color="Amp*"),
+        labels=dict(x="sta_scat_phase", y="evt_scat_phase", color="Median Amp*"),
         aspect="auto",
-        color_continuous_scale="PuBu",)
+        color_continuous_scale="PuBu",)#"PuBu"
 else:
     fig = px.imshow(
         mat2, #mat2 #np.log10(mat + 1)
@@ -202,8 +231,8 @@ fig.update_layout(
     xaxis_tickfont=dict(size=14),
     yaxis_tickfont=dict(size=14),)
 fig.update_layout(width=400, height=800)
-# fig.update_layout(
-#     title="Phase pair counts (evt_scat_phase vs sta_scat_phase)",
-#     xaxis_side="top",)
+fig.update_layout(
+    title="Median Amp*amp wrt P expl",
+    xaxis_side="bottom",)
 # fig.write_image("freq_phases", format="png")
 fig.show()
