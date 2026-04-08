ce7c146ed8ab3d6bd9c3bd7e8760ba6a25f3e9f2
shubham-688 <agr.shubh.agr@gmail.com>
2026-04-08 14:48:25 -0400

    edit to accomodate making density grids w/ just P*P and rest!

--- read_plot_swat/3d_histo_scat.py
+++ read_plot_swat/3d_histo_scat.py
@@ -3,45 +3,60 @@
 import sys
 import numpy as np
 import pandas as pd
-from obspy.taup import TauPyModel
-from obspy.geodetics.base import gps2dist_azimuth, kilometers2degrees
+# from obspy.taup import TauPyModel
+# from obspy.geodetics.base import gps2dist_azimuth, kilometers2degrees
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-csv_path = "/Users/keyser/Research/sct_wat/scattererwhereartthou/examples/swat_230402_180411_all_grids.csv"
+# csv_path = "/Users/keyser/Research/sct_wat/scattererwhereartthou/examples/swat_230402_180411_all_grids.csv"
+csv_path='230402_180411_reso_all_grid_baz5_all.csv'
+
 df = pd.read_csv(csv_path)
 evt_lat = float(df["evtlat"].iloc[0]);  evt_lon = float(df["evtlon"].iloc[0]);  evt_z = float(df["evtdepth"].iloc[0])
 sta_lat = float(df["stalat"].iloc[0]);  sta_lon = float(df["stalon"].iloc[0]);  sta_z = 0.0
 
-model = TauPyModel(model="iasp91")
-lat = df["scatlat"].to_numpy()
-lon = df["scatlon"].to_numpy()
-dep = df["scatdepth"].to_numpy()
+print('Len of read csv:',len(df),'\n')
+df = df.drop_duplicates().reset_index(drop=True)
+print('Len of unique scats:',len(df),'\n')
 
+# model = TauPyModel(model="iasp91")
+
+P_only=['P','Ped']
+
+mask_P = df["evt_scat_phase"].isin(P_only) & df["sta_scat_phase"].isin(P_only)
+
+df_P = df[mask_P]
+df_rest = df[~mask_P]
+
+df_use=df_rest
+
+lat = df_use["scatlat"].to_numpy()
+lon = df_use["scatlon"].to_numpy()
+dep = df_use["scatdepth"].to_numpy()
 lon360 = np.mod(lon, 360.0)
 
-
+# sys.exit()
 ###create bins
 LAT_MIN, LAT_MAX = -30.0, 50.0
 LON_MIN_360, LON_MAX_360 = 90.0, 300.0
-DLAT = 0.2
-DLON = 0.2
+DLAT = 2
+DLON = 2
 
 Z_MIN, Z_MAX = 50.0, 2850.0
-DZ = 100.0
+DZ = 200.0
 
-##
-m = ((lat >= LAT_MIN) & (lat <= LAT_MAX) &
-    (lon360 >= LON_MIN_360) & (lon360 <= LON_MAX_360) &
-    (dep >= Z_MIN) & (dep <= Z_MAX))
-
-lat_f = lat[m]
-lon_f = lon360[m]
-dep_f = dep[m]
-
-print(f"Kept {lat_f.size:,} / {lat.size:,} scatterers in window")
+## bit to keep scaterers within a lat long window
+# m = ((lat >= LAT_MIN) & (lat <= LAT_MAX) &
+#     (lon360 >= LON_MIN_360) & (lon360 <= LON_MAX_360) &
+#     (dep >= Z_MIN) & (dep <= Z_MAX))
+#
+# lat_f = lat[m]
+# lon_f = lon360[m]
+# dep_f = dep[m]
+#
+# print(f"Kept {lat_f.size:,} / {lat.size:,} scatterers in window")
 
 # -------------------------
 # BIN EDGES
@@ -51,7 +66,7 @@ lon_edges = np.arange(LON_MIN_360, LON_MAX_360 + DLON, DLON)
 # depth bins: 50–2850 every 100 km
 dep_edges = np.arange(Z_MIN, Z_MAX + DZ + 1e-9, DZ)
 
-samples = np.column_stack([lat_f, lon_f, dep_f])
+samples = np.column_stack([lat, lon, dep])
 counts, edges = np.histogramdd(samples, bins=[lat_edges, lon_edges, dep_edges])
 
 counts = counts.astype(np.uint32)
@@ -61,8 +76,8 @@ print("total counted:", int(counts.sum()))
 ###########
 #### CHOOSE DEPTH HERE..
 z_target = 550.0
-for z_target in np.arange(50,1350,100):
-# for z_target in [250]:
+# for z_target in np.arange(50,1350,200):
+for z_target in [250]:
 
     k = np.searchsorted(dep_edges, z_target, side="right") - 1
     if k < 0 or k >= counts.shape[2]:
@@ -96,10 +111,10 @@ for z_target in np.arange(50,1350,100):
     ax.add_feature(cfeature.COASTLINE, linewidth=0.5, zorder=10,color='white')
 
     cb = plt.colorbar(m, ax=ax, orientation="horizontal", pad=0.04, fraction=0.06)
-    cb.set_label("Raw scatterer count per 0.2°×0.2°×100 km bin")
+    cb.set_label("Raw scatterer count per 2°×2°×200 km bin")
 
     ax.set_title(f"Scatterer density at ~{z_target:.0f} km")
-    plt.savefig("figs_samples_depth/230402_180411_{}km.png".format(z_target), dpi=300, bbox_inches='tight', pad_inches=0.1)
+    plt.savefig("figs_samples_depth/230402_180411_{}km_rest.png".format(z_target), dpi=300, bbox_inches='tight', pad_inches=0.1)
     plt.close()
 
 plt.show()
